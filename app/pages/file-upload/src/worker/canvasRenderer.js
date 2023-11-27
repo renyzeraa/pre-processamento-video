524501e674ed0a2ea7f0f9e69ca7cc12eb8e1a19
Renan Silva <renansilvaytB@gmail.com>
2023-11-27 19:11:49 -0300

    get performance video processor

--- app/pages/file-upload/src/worker/canvasRenderer.js
+++ app/pages/file-upload/src/worker/canvasRenderer.js
@@ -1,40 +1,42 @@
-/** @param {HTMLCanvasElement} canvas */
-let _canvas = null;
-let _ctx    = null;
+let _canvas
+let _ctx
 export default class CanvasRenderer {
-  /** @param {VideoFrame} frame */
-  static draw(frame) {
-    const {displayHeight, displayWidth } = frame;
-    _canvas.width = displayWidth;
-    _canvas.height = displayHeight;
-    _ctx.drawImage(
-      frame,
-      0,
-      0,
-      displayWidth,
-      displayHeight
-    )
-    frame.close();
-  }
 
-  static getRenderer(canvas) {
-    canvas = canvas;
-    _ctx = _canvas.getContext("2d");
-    const renderer = this;
-    let pendingFrame = null;
-    return frame => {
-      const renderAnimationFrame = () => {
-        renderer.draw(pendingFrame);
-        pendingFrame = null;
-      }
-      if(!pendingFrame){
-        requestAnimationFrame(renderAnimationFrame);
-      }
-      else {
-        pendingFrame.close();  
-      }
+    /** @param {VideoFrame} frame */
+    static draw(frame) {
+        const { displayHeight, displayWidth } = frame
 
-      pendingFrame = frame;
+        _canvas.width = displayWidth
+        _canvas.height = displayHeight
+        _ctx.drawImage(
+            frame,
+            0,
+            0,
+            displayWidth,
+            displayHeight
+        )
+        frame.close()
     }
-  }
+
+    static getRenderer(canvas) {
+        const renderer = this
+        let pendingFrame = null
+        _canvas = canvas
+        _ctx = canvas.getContext('2d')
+
+        return frame => {
+            const renderAnimationFrame = () => {
+                renderer.draw(pendingFrame)
+                pendingFrame = null
+            }
+
+            if (!pendingFrame) {
+                requestAnimationFrame(renderAnimationFrame)
+            } else {
+                pendingFrame.close()
+            }
+
+            pendingFrame = frame;
+        }
+    }
 }
--- app/pages/file-upload/src/worker/worker.js
+++ app/pages/file-upload/src/worker/worker.js
@@ -1,53 +1,50 @@
-import VideoProcessor from './videoProcessor.js';
-import Mp4Demuxer from './mp4Demuxer.js';
-import CanvasRenderer from './canvasRenderer.js';
+import CanvasRenderer from "./canvasRenderer.js"
+import MP4Demuxer from "./mp4Demuxer.js"
+import VideoProcessor from "./videoProcessor.js"
 
 const qvgaConstraints = {
-  width: 320,
-  height: 240 
-};
-
+    width: 320,
+    height: 240
+}
 const vgaConstraints = {
-  width: 640,
-  height: 480 
-};
-
+    width: 640,
+    height: 480
+}
 const hdConstraints = {
-  width: 1280,
-  height: 720
-};
-
-const encoderConfig = {
-  ...qvgaConstraints,
-  bitrate: 10e6,
-  //webm
-  codec: 'vp09.00.10.08',
-  pt: 4,
-  hardwareAcceleration: 'prefer-software',
-  
-  //mp4
-  // codec: 'avc1.42002A',
-  // pt: 1,
-  // hardwareAcceleration: 'hardware',
-
-  // avc: {
-  //   format: 'annexb'
-  // }
+    width: 1280,
+    height: 720
 }
 
-const mp4Demuxer = new Mp4Demuxer()
-const videoProcessor = new VideoProcessor({
-  mp4Demuxer
-});
+const encoderConfig = {
+    ...qvgaConstraints,
+    bitrate: 10e6,
+    // WebM
+    codec: 'vp09.00.10.08',
+    pt: 4,
+    hardwareAcceleration: 'prefer-software',
 
-onmessage = async ({data}) => {
-  const renderFrame = CanvasRenderer.getRenderer(data.canvas);
-  await videoProcessor.start({
-      file: data.file,
-      renderFrame,
-      encoderConfig,
-      sendMessage(message){
-        self.postMessage(message)
-      }
-  });
+    // MP4
+    // codec: 'avc1.42002A',
+    // pt: 1,
+    // hardwareAcceleration: 'prefer-hardware',
+    // avc: { format: 'annexb' }
+}
+
+const mp4Demuxer = new MP4Demuxer()
+const videoProcessor = new VideoProcessor({
+    mp4Demuxer
+})
+
+
+onmessage = async ({ data }) => {
+    const renderFrame = CanvasRenderer.getRenderer(data.canvas)
+    await videoProcessor.start({
+        file: data.file,
+        renderFrame,
+        encoderConfig,
+    })
+
+    self.postMessage({
+        status: 'done'
+    })
 }
